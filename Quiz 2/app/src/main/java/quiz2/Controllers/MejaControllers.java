818271666e428e5699ad0ffcabf7c05ca0aaf876
Marvel Nathanael <marvelnathanael072006@gmail.com>
2025-10-13 13:23:53 +0700

    update

--- Quiz 2/app/src/main/java/quiz2/Controllers/MejaControllers.java
+++ Quiz 2/app/src/main/java/quiz2/Controllers/MejaControllers.java
@@ -3,12 +3,35 @@ package quiz2.Controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import quiz2.Models.Bahan;
 import quiz2.Models.Furniture;
+import quiz2.Models.Meja;
 
-public class ObjectControllers {
+public class MejaControllers {
 
-    private List<Furniture> listFurniture = new ArrayList<>();
-    public static int InputNewObject(){
-        return 1;
+    private static List<Meja> listMeja = new ArrayList<>();
+
+    public static void InputNewObject(String[] inputBeratDanSandaran, String bahan){
+        Bahan bahanUtama = null;
+        String berat = inputBeratDanSandaran[0];
+        if (bahan.equalsIgnoreCase("besi")) {
+            bahanUtama = bahanUtama.BESI;
+        } else if (bahan.equalsIgnoreCase("kayu")) {
+            bahanUtama = bahanUtama.KAYU;
+        } else {
+            bahanUtama = bahanUtama.PLASTIK;
+        }
+
+        boolean adaSandaran;
+        adaSandaran = !inputBeratDanSandaran[1].equalsIgnoreCase("n");
+
+        Meja meja = new Meja(berat, bahanUtama, adaSandaran);
+        listMeja.add(meja);
+    }
+
+    public static void PrintAllKursi(){
+        for (Meja meja : listMeja) {
+            System.out.println(meja);
+        }
     }
 }
